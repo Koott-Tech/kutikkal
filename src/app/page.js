6658ec1c8df8d76b4949043689bc83aec6bf964d
AbhishekRavi063 <abhishekravi063@gmail.com>
2025-08-09 13:10:15 +0530

    added logo

--- src/app/page.js
+++ src/app/page.js
@@ -58,6 +58,34 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* Logos strip */}
+      <section className="mt-10">
+
+        <div className="mt-4">
+          <div className="flex flex-nowrap items-center justify-center gap-10 px-[50px] pb-2">
+            {[
+              "aetna-logo-1.png",
+              "cigna-logo-1.png",
+              "kaiser-logo.png",
+              "logo_bcbs-1.png",
+              "optum-logo-1.png",
+              "uhc-logo.png",
+              "umr-logo.png",
+            ].map((src) => (
+              <div key={src} className="relative h-14 w-36 shrink-0">
+                <Image
+                  src={`/${src}`}
+                  alt={src.replace(/[-_]/g, " ").replace(/\.png$/, "")}
+                  fill
+                  className="object-contain"
+                  sizes="112px"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
